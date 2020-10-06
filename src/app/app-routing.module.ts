e746de9d13dd91767c2e809cf97fd876a6ca7356
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-06 13:31:22 +0300

    add routing the Profile compnent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { from } from 'rxjs';
+import {ProfileComponent} from 'src/app/components/profile/profile.component'
 
 
 const routes: Routes = [
-
+  { path: 'profile', component: ProfileComponent}
   
 ];
 
